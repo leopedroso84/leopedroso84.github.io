50dd09b738bafed545c5fbdb01774c06a608ef4e
Leandro Augusto Pedroso de Souza <68203197+leopedroso84@users.noreply.github.com>
2026-09-02 12:43:05 -0300

    Remove temporary shortcut fetch helper

--- .shortcut_fetch_temp.py
+++
@@ -1,6 +0,0 @@
-import json, urllib.request
-sid='994ba242ad604eedb136dc89355b9af8'
-url='https://www.icloud.com/shortcuts/api/records/'+sid
-with urllib.request.urlopen(url, timeout=30) as r:
-    data=json.load(r)
-print(data['fields']['name']['value'])
